66f7584436a4439ebe25677162f8b0c43efa853e
TakumiNatsuka <natsukatakumi@outlook.jp>
2023-07-11 20:48:45 +0900

    追加:キャラの4方向移動、アニメーション。 課題:キー入力時のカクつき、斜め移動ができない

---
+++ stg_toho/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stg_toho
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        int time = 0,m;
+        jiki jiki = new jiki();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                jiki.MoveUp();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                jiki.MoveDown();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                jiki.MoveLeft();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                jiki.MoveRight();
+            }
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            jiki.Draw(time,e);   
+        }
+    }
+}
---
+++ stg_toho/Jiki.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stg_toho
+{
+    class jiki
+    {
+        private int speed;
+        private Point position;
+        Image image = Resource1.Minoriko;
+        Rectangle[,] trimRects = new Rectangle[4, 3];
+
+        public jiki()
+        {
+            speed = 10;
+            position = new Point(0, 0);
+            int width = image.Width / trimRects.GetLength(0), height = image.Height / trimRects.GetLength(1);
+            for (int i = 0; i < trimRects.GetLength(0); i++) for (int j = 0; j < trimRects.GetLength(1); j++)
+                    trimRects[i, j] = new Rectangle(i * width, j * height, width, height);
+        }
+
+        public void MoveUp()
+        {
+            position.Y -= speed;
+        }
+
+        public void MoveDown()
+        {
+            position.Y += speed;
+        }
+
+        public void MoveRight()
+        {
+            position.X += speed;
+        }
+
+        public void MoveLeft()
+        {
+            position.X -= speed;
+        }
+
+
+
+        public void Draw(int time,PaintEventArgs e)
+        {
+            var n = (time / 10) % trimRects.GetLength(0);
+            var m = (time / 100) % trimRects.GetLength(1);
+            e.Graphics.DrawImage(image, new Rectangle(position.X, position.Y, 50, 50), trimRects[n, m], GraphicsUnit.Pixel);
+        }
+
+
+    }
+}
